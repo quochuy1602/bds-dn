de499a3ca1245c4b0289a4f4f3204c856642c68e
quochuy1602 <quochuy1602@gmail.com>
2019-02-18 15:19:10 +0700

    add polygon

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -5,10 +5,37 @@ import {
     withScriptjs,
     withGoogleMap,
     GoogleMap,
-    Marker
+    Marker,
+    Polygon
 } from "react-google-maps";
 
+const coords = [
+    { lat: 29.047487, lng: 41.023164 },
+    { lat: 29.0459633, lng: 41.0212904 },
+    { lat: 29.0449333, lng: 41.0167573 },
+    { lat: 29.0393543, lng: 41.0106695 },
+    { lat: 29.032917, lng: 41.0049697 },
+    { lat: 29.0226173, lng: 41.0061356 },
+    { lat: 29.0078545, lng: 41.0039334 },
+    { lat: 29.0201283, lng: 40.9765933 },
+    { lat: 29.0319729, lng: 40.9657708 },
+    { lat: 29.0784073, lng: 40.9536501 },
+    { lat: 29.0944576, lng: 40.9493068 },
+    { lat: 29.0975475, lng: 40.9514461 },
+    { lat: 29.1052294, lng: 40.9647986 },
+    { lat: 29.097338, lng: 40.978242 },
+    { lat: 29.0931273, lng: 40.9835914 },
+    { lat: 29.0858746, lng: 40.987738 },
+    { lat: 29.056509, lng: 40.998902 },
+    { lat: 29.061456, lng: 41.008443 },
+    { lat: 29.0617561, lng: 41.0104752 },
+    { lat: 29.0595245, lng: 41.0126772 },
+    { lat: 29.052014, lng: 41.018198 },
+    { lat: 29.047487, lng: 41.023164 }];
 
+const reversedCoords = coords.map( ll => {
+    return { lat: ll.lng, lng: ll.lat }
+});
 const MyMapComponent = compose(
     withProps({
         googleMapURL:
@@ -20,8 +47,17 @@ const MyMapComponent = compose(
     withScriptjs,
     withGoogleMap
 )(props => (
-    <GoogleMap defaultZoom={13} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
-        <Marker position={{ lat: -34.397, lng: 150.644 }} />
+    <GoogleMap defaultZoom={8} defaultCenter={{ lat: 41.0104752 , lng: 29.0617561  }}>
+        <Polygon
+            path={reversedCoords}
+            //key={1}
+            options={{
+                fillColor: "#000",
+                fillOpacity: 0.4,
+                strokeColor: "#000",
+                strokeOpacity: 1,
+                strokeWeight: 1
+            }} />
     </GoogleMap>
 ));
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -36,7 +36,6 @@ class Navbar extends Component {
         )
         return(
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <Link className="navbar-brand" to="/">Redux Node Auth</Link>
                 <Link className="navbar-brand" to="/map">Map</Link>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     {isAuthenticated ? authLinks : guestLinks}
